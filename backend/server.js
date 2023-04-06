b61618c92ac67e78bea24d16bcc0ea6ff10c9537
jronnomo <ggronnii@gmail.com>
2023-04-06 12:02:02 -0400

    added user authentication to API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
+const { protect } = require('./middleware/authMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
@@ -13,6 +14,9 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes.js'))
 app.use(errorHandler)
+//did include?
+app.use(protect)
 
 app.listen(port, ()=> console.log(`Server started on ${port}`))
