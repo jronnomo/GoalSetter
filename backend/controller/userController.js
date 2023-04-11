6dbc2d28a140ddc65039bca414f2c90f41b4ebe1
jronnomo <ggronnii@gmail.com>
2023-04-11 08:49:58 -0400

    Update userController.js

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -75,15 +75,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/users/me
 //@access Private
 
-const getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id);
-
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
-});
+const getMe = asyncHandler(async (req, res) => { res.status(200).json(req.user) });
 
 //@desc Generate JSON Web Token
 const generateToken = (id) => {
